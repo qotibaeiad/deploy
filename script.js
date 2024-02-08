ff0ecdc6d69989e673e6b9a9219923944213e8af
user <user@users-MacBook-Pro.local>
2024-02-08 11:08:51 +0200

    fix abdullah login not typing passwordk or eamil

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ document.addEventListener('DOMContentLoaded', function () {
   loadPage('gridlnews.html');
 });
 
+function checkLogin() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if (username === 'abdallh' && password === '123456') {
+     
+      window.location.href = 'https://www.ynet.co.il/home/0,7340,L-8,00.html';
+  } else {
+      alert('Incorrect username or password');
+  }
+}
+
 function loadPage(page) {
   // Use AJAX to load the content of the specified HTML page
   $.ajax({
